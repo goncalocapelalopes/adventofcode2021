0e8c2f14ab2edaeab3310aeade27bb007f78ed1e
goncalocapelalopes <goncalo.capela.lopes@gmail.com>
2022-04-04 10:54:16 +0100

    Day 20 Solution.

--- Day 20/part_1_sol.py
+++ Day 20/part_1_sol.py
@@ -1,8 +1,8 @@
 PUZZLE_INPUT_FILE = "puzzle_input.txt"
 
-def pad_image(image):
-    image = [".." + line + ".." for line in image]
-    image = ["."*len(image[0])] * 2 + image + ["."*len(image[0])] * 2
+def pad_image(image, char):
+    image = [char*2 + line + char*2 for line in image]
+    image = [char*len(image[0])] * 2 + image + [char*len(image[0])] * 2
     return image
 
 def pixels2number(pixels):
@@ -10,15 +10,9 @@ def pixels2number(pixels):
     return int(binary, 2)
 
 def count_lit_pixels(image):
-    total_sum = 0
-    for row in image:
-        for c in row:
-            if c == "#":
-                total_sum += 1
-    return total_sum
-    #return sum([sum([1 if p=="#" else 0 for p in row]) for row in image])
+    return sum([sum([1 if p=="#" else 0 for p in row]) for row in image])
 
-def enhance_image(image, algorithm):
+def enhance_image(image, algorithm, last_pad):
     new_image = []
     for i in range(1, len(image)-1):
         row = ""
@@ -28,7 +22,14 @@ def enhance_image(image, algorithm):
             new_pixel = algorithm[number]
             row += new_pixel
         new_image.append(row)
-    return pad_image(new_image)
+    if last_pad == "." and algorithm[0] == "#":
+        pad_char = "#"
+    elif last_pad == "#" and algorithm[-1] == ".":
+        pad_char = "."
+    else:
+        pad_char = last_pad
+    
+    return pad_image(new_image, pad_char), pad_char
 
 if __name__ == "__main__":
     lines = open(PUZZLE_INPUT_FILE, "r").readlines()
@@ -40,13 +41,14 @@ if __name__ == "__main__":
             algorithm += line.strip("\n")
     # Create padded image
     image = [line.strip("\n") for line in lines[i+1:]]
-    image = pad_image(image)
-    
-    print(algorithm)
+    image = pad_image(image, ".")
+    last_pad = "."
     print("Lit pixels ->", count_lit_pixels(image))
-    for i in range(2):
-        
-        image = enhance_image(image, algorithm)
+    for _ in range(50):
+        image, last_pad = enhance_image(image, algorithm, last_pad)
         for row in image:
             print(row)
-        print("Lit pixels ->", count_lit_pixels(image))
+        if last_pad == "#":
+            print("Lit pixels -> infinite")
+        else:
+            print("Lit pixels ->", count_lit_pixels(image))
